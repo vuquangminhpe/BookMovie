20c99ccc1dc1d26174026b11184b2975dfcb805b
vuquangminhpe <minhvqhe176726@fpt.edu.vn>
2025-07-15 01:22:13 +0700

    fix bug test

--- src/services/booking.services.ts
+++ src/services/booking.services.ts
@@ -65,6 +65,7 @@ class BookingService {
         status: HTTP_STATUS.NOT_FOUND
       })
     }
+    console.log(showtime.status)
 
     // Verify showtime is available for booking
     if (showtime.status !== ShowtimeStatus.BOOKING_OPEN) {
@@ -75,7 +76,7 @@ class BookingService {
     }
 
     // Verify showtime hasn't started yet
-    if (showtime.start_time > new Date()) {
+    if (new Date(showtime.start_time) < new Date()) {
       throw new ErrorWithStatus({
         message: BOOKING_MESSAGES.SHOWTIME_NOT_AVAILABLE,
         status: HTTP_STATUS.BAD_REQUEST
